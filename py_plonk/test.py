40c9183ef2dbd00a89b0f762596c56204efc4102
Ioannis Kaklamanis <jkaklamanis@gmail.com>
2024-10-09 23:32:05 -0400

    added encryption test for cs517 hw2

--- py_plonk/test.py
+++ py_plonk/test.py
@@ -96,6 +96,112 @@ def factorization_test(setup):
     assert v.verify_proof(setup, 16, vk, proof, public, optimized=True)
     print("Factorization test success!")
 
+def encryption_test(setup):
+    print("Beginning test: prove you know the plaintext of an encrypted message")
+
+    n = 8
+
+    eqs = """
+            i public
+    """
+    for ii in range(0, n):
+        public_ii = f"""
+                a{ii}0 public
+                a{ii}1 public
+                a{ii}2 public
+                b{ii} public
+            """
+        eqs += public_ii
+    
+    eqs += """p public"""
+
+    i_stuff = """
+        i0 === i0 * i0
+        i1 === i1 * i1
+        i2 === i2 * i2
+        ii01 <== i0 + 2 * i1
+        i === ii01 + 4 * i2
+        """
+    i2bin = {0 : "000", 1 : "001", 2 : "010", 3 : "011", 4 : "100", 5 : "101", 6 : "110", 7 : "111"}
+
+    for ii in range(0, n):
+        i_stuff += f"""
+                    i{ii}0 === {i2bin[ii][2]}
+                    i{ii}1 === {i2bin[ii][1]}
+                    i{ii}2 === {i2bin[ii][0]}
+                    i{ii}0bar <== 1 - i{ii}0
+                    i{ii}1bar <== 1 - i{ii}1
+                    i{ii}2bar <== 1 - i{ii}2
+                    i{ii}0diff <== i{ii}0bar - i0
+                    i{ii}1diff <== i{ii}1bar - i1
+                    i{ii}2diff <== i{ii}2bar - i2
+                    i{ii}01prod <== i{ii}0diff * i{ii}1diff
+                    i{ii}012prod <== i{ii}01prod * i{ii}2diff
+                    i{ii}sel <== i{ii}012prod * i{ii}012prod
+                """
+    
+    s_stuff = """
+        s0 === s0 * s0
+        s1 === s1 * s1
+        s2 === s2 * s2
+        p === pp
+    """
+
+    a_stuff = """"""
+    for ii in range(0, n):
+        a_stuff += f"""
+                    a{ii}0 === aa{ii}0
+                    a{ii}1 === aa{ii}1
+                    a{ii}2 === aa{ii}2
+                    b{ii} === bb{ii}
+                    a{ii}0s0 <== aa{ii}0 * s0
+                    a{ii}1s1 <== aa{ii}1 * s1
+                    a{ii}2s2 <== aa{ii}2 * s2
+                    as{ii}01 <== a{ii}0s0 + a{ii}1s1
+                    a{ii}s <== as{ii}01 + a{ii}2s2
+                    a{ii}splusp <== a{ii}s + pp
+                    bsel{ii} <== bb{ii} * i{ii}sel
+                    aspluspsel{ii} <== a{ii}splusp * i{ii}sel
+                    bsel{ii} <== aspluspsel{ii}
+                """
+    eqs += i_stuff + s_stuff + a_stuff
+    
+
+    public = [  1,
+                3, 7, 11, 15,
+                8, 2, 5, 19, 
+                5, 7, 2, 11,
+                3, 1, 1, 8,
+                2, 0, 10, 10,
+                6, 12, 3, 5,
+                5, 9, 3, 11,
+                1, 0, 7, 5,
+                6
+            ]
+    group_order = 64 * n
+    vk = c.make_verification_key(setup, group_order, eqs)
+    print("Generated verification key")
+    assignments = c.fill_variable_assignments(eqs, {
+        'i0' : 1, 'i1' : 0, 'i2' : 0,
+        'aa00': 3, 'aa01': 7, 'aa02': 11, 'bb0': 15,
+        'aa10': 8, 'aa11': 2, 'aa12': 5, 'bb1': 19,
+        'aa20': 5, 'aa21': 7, 'aa22': 2, 'bb2': 11,
+        'aa30': 3, 'aa31': 1, 'aa32': 1, 'bb3': 8,
+        'aa40': 2, 'aa41': 0, 'aa42': 10, 'bb4': 10,
+        'aa50': 6, 'aa51': 12, 'aa52': 3, 'bb5': 5,
+        'aa60': 5, 'aa61': 9, 'aa62': 3, 'bb6': 11,
+        'aa70': 1, 'aa71': 0, 'aa72': 7, 'bb7': 5,
+        's0': 1, 's1': 0, 's2': 1, 
+        'pp': 6
+    })
+    proof = p.prove_from_witness(setup, group_order, eqs, assignments)
+    print("Generated proof")
+    assert v.verify_proof(setup, group_order, vk, proof, public, optimized=True)
+    print("Encryption test success!")
+
+
+         
+
 def output_proof_lang():
     o = []
     o.append('L0 public')
@@ -138,9 +244,10 @@ def poseidon_test(setup):
     print("Verified proof!")
 if __name__ == '__main__':
     setup = basic_test()
-    ab_plus_a_test(setup)
-    one_public_input_test(setup)
-    proof = prover_test(setup)
-    verifier_test(setup, proof)
-    factorization_test(setup)
-    poseidon_test(setup)
+    # ab_plus_a_test(setup)
+    # one_public_input_test(setup)
+    # proof = prover_test(setup)
+    # verifier_test(setup, proof)
+    # factorization_test(setup)
+    encryption_test(setup)
+    # poseidon_test(setup)
